e1483bd82439df1096a0904824baf67b29c0cb1b
0xKhedr <0xkhedr@gmail.com>
2025-10-08 18:40:55 +0300

    Run pint

--- src/Receivers/ReceiverAbstract.php
+++ src/Receivers/ReceiverAbstract.php
@@ -5,8 +5,39 @@ namespace Raid\Caller\Receivers;
 use Raid\Caller\Traits\Arrayable;
 use Raid\Caller\Traits\Responsable;
 
-abstract class ReceiverAbstract implements Contracts\Receiver
+abstract readonly class ReceiverAbstract implements Contracts\Receiver
 {
     use Arrayable;
     use Responsable;
+
+    public function __construct(
+        protected int $status,
+    ) {}
+
+    public function toSuccessResponse(): array
+    {
+        return [];
+    }
+
+    public function toErrorResponse(): array
+    {
+        return [];
+    }
+
+    public function toResponse(): array
+    {
+        return $this->isSuccessResponse()
+            ? $this->toSuccessResponse()
+            : $this->toErrorResponse();
+    }
+
+    public function isSuccessResponse(): bool
+    {
+        return $this->status >= 200 && $this->status < 300;
+    }
+
+    public function getStatus(): int
+    {
+        return $this->status;
+    }
 }
